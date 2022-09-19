cf3cbf9e682285c04c18b91e9548ebd4ba22d891
Ana Maria Arbelaez L <anam.arbelaezlopez@gmail.com>
2022-09-19 15:47:29 -0500

    fixed views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  before_action :set_article, only: %i[show edit update destroy]
 
   # GET /articles
   def index
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1
   def destroy
     @article.destroy
-    redirect_to articles_url, notice: "Article was successfully destroyed."
+    redirect_to root_path, notice: "Article was successfully destroyed."
   end
 
   private
